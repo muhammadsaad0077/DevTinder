02e708cdef041cda4a54f3b1085a6350560f2fb3
muhammadsaad0077 <saaditanoli12@gmail.com>
2024-10-23 13:41:06 +0500

    Creating_route

--- src/app.js
+++ src/app.js
@@ -1,2 +1,26 @@
-console.log("Creating our first project");
+const express = require('express');
+const app = express();
+
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to the Home Page");
+})
+
+app.get("/login", (req, res)=>{
+    res.send("Welcome to the Login Page");
+})
+
+app.get("/hello", (req, res)=>{
+    res.send("Welcome to the Hello Page");
+})
+
+app.get("/register", (req, res)=>{
+    res.send("Welcome to the Register Page");
+})
+
+
+
+app.listen(3001, ()=>{
+console.log("Server is successfully listening on 3000"); 
+})
 

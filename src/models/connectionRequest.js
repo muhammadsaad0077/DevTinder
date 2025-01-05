19f8df8c2584512354d5c677829a0ef20c69c1ee
muhammadsaad0077 <saaditanoli12@gmail.com>
2025-01-05 17:53:12 +0500

    Connection Request

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ["ignored", "intrested", "accepted", "rejected"],
+            message: `{VALUE} is not a valid status`,
+        },
+        required: true
+    }
+}, { timestamps: true })
+
+const ConnectionRequestModel = new mongoose.model("ConnectionRequestModel", connectionRequestSchema);
+module.exports = ConnectionRequestModel;

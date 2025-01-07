7bf80240eeee5b1cc3d7cf18887082ab7ab9e455
muhammadsaad0077 <saaditanoli12@gmail.com>
2025-01-07 18:54:26 +0500

    connection-request-3

--- src/router/connection.js
+++ src/router/connection.js
@@ -14,9 +14,41 @@ connectionRouter.post('/connection/send/:status/:toUserId', userAuth, async(req,
     const toUserId = req.params.toUserId;
     const status = req.params.status;
 
-    const recieverUserName = await User.findById(toUserId);
-    console.log(recieverUserName);
+
+    const allowedStatus = ["interested", "ignored"];
+
+    if(!allowedStatus.includes(status)){
+      throw new Error("Invalid status type")
+    }
+
+    if(fromUserId === toUserId){
+      throw new Error("You can't send a request to yourself")
+    }
+
+    const recieverUserName = await User.findById(toUserId); 
+
+    if(!recieverUserName){
+      throw new Error("User not found");
+    }
+
     
+
+    const existingRequest = await ConnectionRequestModel.findOne({
+      $or: [
+      {
+      fromUserId: fromUserId,
+      toUserId: toUserId
+      },
+      {
+        fromUserId: toUserId,
+        toUserId: fromUserId
+      }
+      ]
+    })
+
+    if(existingRequest){
+      throw new Error("Connection request already exist")
+    }
 
     const connectionRequest = new ConnectionRequestModel({
       fromUserId: fromUserId,
@@ -25,13 +57,26 @@ connectionRouter.post('/connection/send/:status/:toUserId', userAuth, async(req,
     })
 
     const data = await connectionRequest.save();
-  
+
+    if(status=="interested"){
     res.json({message: `${user.firstName} sent a request to ${recieverUserName.firstName}`, data});
   }
+  else{
+    res.json({message: `${user.firstName} is not interested in ${recieverUserName.firstName}`, data});
+  }
+}
+
   catch(err){
     res.status(400).send(`Error: ${err.message}`)
   }
   
   })
+
+
+connectionRouter.post('/connection/request/:status/:userId', userAuth, async(req, res)=>{
+
+
+
+})
 
   module.exports = connectionRouter;

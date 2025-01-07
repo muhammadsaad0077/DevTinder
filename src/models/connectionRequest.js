3447ea15f584bf1c442f79894a52490fd583910f
muhammadsaad0077 <saaditanoli12@gmail.com>
2025-01-07 13:05:30 +0500

    connection-request-2

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -12,7 +12,7 @@ const connectionRequestSchema = new mongoose.Schema({
     status: {
         type: String,
         enum: {
-            values: ["ignored", "intrested", "accepted", "rejected"],
+            values: ["ignored", "interested", "accepted", "rejected"],
             message: `{VALUE} is not a valid status`,
         },
         required: true
--- src/router/connection.js
+++ src/router/connection.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const connectionRouter = express.Router();
-const {userAuth} = require('../middlewares/authAdmin')
+const {userAuth} = require('../middlewares/authAdmin');
+const ConnectionRequestModel = require('../models/connectionRequest');
+const User = require('../models/user')
 
 
-connectionRouter.post('/connection', userAuth, (req, res)=>{
+connectionRouter.post('/connection/send/:status/:toUserId', userAuth, async(req, res)=>{
 
+  try{
     const user = req.user;
+    const id = user._id;
+    const fromUserId = id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
+
+    const recieverUserName = await User.findById(toUserId);
+    console.log(recieverUserName);
+    
+
+    const connectionRequest = new ConnectionRequestModel({
+      fromUserId: fromUserId,
+      toUserId: toUserId,
+      status: status
+    })
+
+    const data = await connectionRequest.save();
   
-    res.send(`${user.firstName} Connection Request Send`);
+    res.json({message: `${user.firstName} sent a request to ${recieverUserName.firstName}`, data});
+  }
+  catch(err){
+    res.status(400).send(`Error: ${err.message}`)
+  }
   
   })
 

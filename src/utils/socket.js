2d2f9cc16e169b8c135c21fc5b4e42a07f634139
muhammadsaad0077 <saaditanoli12@gmail.com>
2025-03-21 14:53:56 +0500

    working chat

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,33 +1,40 @@
 const socket = require('socket.io')
 
-
 const intializeSocket = (server) =>{
-    const io = socket(server, {
-        cors: {
-            origin: "http://localhost:5173"
-        }       
+  const io = socket(server, {
+      cors: {
+          origin: "*"
+      }       
+    })
+
+    console.log("🔌 Socket.io initialized... Waiting for connections");
+    
+    io.on("connection", (socket)=>{
+      // Handle Connection
+      socket.on("joinChat", ({firstName, userId, targetUserId})=>{
+        const roomId = [userId, targetUserId].sort().join("_")
+        console.log(firstName + " Joined Room: "+roomId);         
+        socket.join(roomId);
+      });
+
+      socket.on("sendMessage", ({firstName, userId, targetUserId, text})=>{
+        console.log("In send message");
+        
+        const roomId = [userId, targetUserId].sort().join("_");
+        console.log(firstName + " Sent Message: "+text);
+        io.to(roomId).emit("meesageReceived", {firstName, text});
+        
+
+      });
+
+      socket.on("disconnect", ()=>{
+        console.log("User disconnected:", socket.id);
       })
-      
-      io.on("connection", (socket)=>{
-        // Handle Connection
-        socket.on("joinChat", ({firstName, userId, targetUserId})=>{
-          const roomId = [userId, targetUserId].sort().join("_")
-          console.log(firstName + " Joined Room: "+roomId);         
-          socket.join(roomId);
-        });
-
-        socket.on("sendMessage", ()=>{
-
-        });
-
-        socket.on("disconnect", ()=>{
-
-        })
 
 
 
 
-      })
+    })
 }
 
 module.exports = intializeSocket;
